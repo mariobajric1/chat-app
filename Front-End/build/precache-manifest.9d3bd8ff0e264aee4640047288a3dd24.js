7606c12cb08f7f513d53d5db81c9c9ecf8e9196c
Mario <Mario@Jadrankos-iMac.local>
2020-10-01 11:20:04 -0500

    deployment

---
+++ Front-End/build/precache-manifest.9d3bd8ff0e264aee4640047288a3dd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fb0deb65004efd20265c1db3b3581e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f417533c20c943b60053",
+    "url": "/static/css/main.757c6e6a.chunk.css"
+  },
+  {
+    "revision": "98c3097b0aa52be86443",
+    "url": "/static/js/2.a0a8a20e.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/static/js/2.a0a8a20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f417533c20c943b60053",
+    "url": "/static/js/main.26947333.chunk.js"
+  },
+  {
+    "revision": "8ee40f9b318c3c875d87",
+    "url": "/static/js/runtime-main.67bfdc7e.js"
+  }
+]);
